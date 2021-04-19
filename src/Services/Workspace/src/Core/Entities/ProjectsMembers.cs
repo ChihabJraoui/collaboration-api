23d6f178413b721a13d1a8f3764095879ca88cd5
Mahdia Saadi <mahdia.saadielhassani@gmail.com>
2021-04-19 14:44:02 +0000

    First commit.

---
+++ src/Services/Workspace/src/Core/Entities/ProjectsMembers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Entities
+{
+    public class ProjectsMembers
+    {
+
+        public int Id { get; set; }
+        public Guid MemberId { get; set; }
+       // public Member Member { get; set; }
+        public Guid ProjectId { get; set; }
+        public Project Project { get; set; }
+
+    }
+}
---
+++ src/Services/Workspace/src/Web/Program.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Web.Configurations;
+
+namespace Web
+{
+    public class Program
+    {
+        public async static Task Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                            .SetBasePath(Directory.GetCurrentDirectory())
+                            .AddJsonFile("appsettings.json")
+                            .AddCommandLine(args)
+                            .Build();
+            var host = CreateHostBuilder(args).Build();
+
+            Log.Logger = LoggerSetup.CreateLogger(config);
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+            try
+                {
+
+                    Log.Information("Starting up");
+                    CreateHostBuilder(args).Build().Run();
+                }
+                catch (Exception ex)
+                {
+                    Log.Fatal(ex, "Application start-up failed");
+                }
+                finally
+                {
+                    Log.CloseAndFlush();
+                }
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .UseSerilog()
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
