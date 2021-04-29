da3423708b64a710fe28e62e3e3be89acb18cfe4
khadija <khadija.akhardid@verbindungszentrum.com>
2021-04-29 16:08:37 +0000

    fix: add  exception middlware and ApiResponseDetails

--- Collaboration.ShareDocs/SourceCode/BusinessLayer/Collaboration.ShareDocs.Application/Common/Exceptions/Error.cs
+++ Collaboration.ShareDocs/SourceCode/BusinessLayer/Collaboration.ShareDocs.Application/Common/Exceptions/Error.cs
@@ -1,19 +1,4 @@
-﻿public class Error
-{
-    public Error()
-    {
-    }
-
-    public Error(string name, string message)
-    {
-        this.Name = name;
-        this.Message = message;
-    }
-
-    public string Name { get; set; }
-
-    public string Message { get; set; }
-}
+﻿
 namespace Collaboration.ShareDocs.Application.Common.Exceptions
 {
     public class Error
