3e7dab0d48d58cfb8e560073eb416341d14400ee
Mahdia Saadi <mahdia.saadielhassani@gmail.com>
2021-05-06 18:30:01 +0000

    create file command

---
+++ Collaboration.ShareDocs/SourceCode/BusinessLayer/Collaboration.ShareDocs.Application/Commands/Files/CreateFileCommand.cs
@@ -0,0 +1,13 @@
+﻿using Collaboration.ShareDocs.Application.Common.Response;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Collaboration.ShareDocs.Application.Commands.Files
+{
+    public class CreateFileCommand:IRequest<ApiResponseDetails>
+    {
+
+    }
+}
