ee616fa680474e3d3002cc2b5ba3e445ea117dc2
Chihab Jraoui <contact@chihabjraoui.com>
2021-06-25 19:57:37 +0100

    Add Created property

--- Collaboration.ShareDocs/SourceCode/BusinessLayer/Collaboration.ShareDocs.Application/Commands/Workspaces/Dto/WorkspaceDto.cs
+++ Collaboration.ShareDocs/SourceCode/BusinessLayer/Collaboration.ShareDocs.Application/Commands/Workspaces/Dto/WorkspaceDto.cs
@@ -3,20 +3,27 @@ using Collaboration.ShareDocs.Application.Common.Mapping;
 using Collaboration.ShareDocs.Persistence.Entities;
 using System;
 using System.Collections.Generic;
-using System.Text;
+using Collaboration.ShareDocs.Application.Commands.Projects.Dto;
 
 namespace Collaboration.ShareDocs.Application.Commands.Workspaces.Dto
 {
     public class WorkspaceDto : IMapForm<Workspace>
     {
         public Guid Id { get; set; }
+
         public string Name { get; set; }
+
         public string Description { get; set; }
+
         public string Image { get; set; }
+
         public string Owner { get; set; }
+
         public bool BookMark { get; set; }
+
         public bool IsPrivate { get; set; }
-        
+
+        public DateTime Created { get; set; }
 
         public virtual void Mapping(Profile profile)
         {
