7bf07f3bf6387d5d75a84b88fe08a51aba95bc0b
Chihab Jraoui <contact@chihabjraoui.com>
2021-06-25 19:57:56 +0100

    Create Workspace details DTO

---
+++ Collaboration.ShareDocs/SourceCode/BusinessLayer/Collaboration.ShareDocs.Application/Commands/Workspaces/Dto/WorkspaceDetailsDto.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using Collaboration.ShareDocs.Application.Common.Mapping;
+using Collaboration.ShareDocs.Persistence.Entities;
+using System;
+using System.Collections.Generic;
+using Collaboration.ShareDocs.Application.Commands.Projects.Dto;
+
+namespace Collaboration.ShareDocs.Application.Commands.Workspaces.Dto
+{
+    public class WorkspaceDetailsDto : IMapForm<Workspace>
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Image { get; set; }
+
+        public string Owner { get; set; }
+
+        public bool BookMark { get; set; }
+
+        public bool IsPrivate { get; set; }
+
+        public DateTime Created { get; set; }
+        
+        public ICollection<ProjectDto> Projects { get; set; }
+
+        public virtual void Mapping(Profile profile)
+        {
+            profile.CreateMap<Workspace, WorkspaceDto>();
+        }
+    }
+}
